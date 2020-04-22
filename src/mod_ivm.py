8f213d3196225c02836491a7e32b05d12f444a0b
Arkhorse <theillusivemman@gmail.com>
2020-04-22 03:34:56 -0300

    add overrideMethod

--- src/mod_ivm.py
+++ src/mod_ivm.py
@@ -380,6 +380,62 @@ def ivm_checkConfig():
 
 ivm_checkConfig()
 
+#@overrideMethod
+def overrideMethod(obj, prop, getter=None, setter=None, deleter=None):
+    """
+    :param obj: object, which attribute needs overriding
+    :param prop: attribute name (can be not mangled), attribute must be callable
+    :param getter: fget function or None
+    :param setter: fset function or None
+    :param deleter: fdel function or None
+    :return function: unmodified getter or, if getter is None and src is not property, decorator"""
+
+    if inspect.isclass(obj) and prop.startswith('__') and prop not in dir(obj) + dir(type(obj)):
+        prop = obj.__name__ + prop
+        if not prop.startswith('_'):
+            prop = '_' + prop
+    src = getattr(obj, prop)
+    if type(src) is property and (getter or setter or deleter):
+        props = []
+        for func, fType in ((getter, 'fget'), (setter, 'fset'), (deleter, 'fdel')):
+            assert func is None or callable(func), fType + ' is not callable!'
+            props.append(partial(func, getattr(src, fType)) if func else getattr(src, fType))
+        setattr(obj, prop, property(*props))
+        return getter
+    elif getter:
+        getter_orig = getter
+        assert callable(src), 'Source property is not callable!'
+        assert callable(getter_orig), 'Handler is not callable!'
+        while isinstance(getter, partial):
+            getter = getter.func
+
+        def getter_new(*a, **k):  # noinspection PyUnusedLocal
+            info = None
+            try:
+                return getter_orig(src, *a, **k)
+            except Exception:  # Code to remove this wrapper from traceback
+                info = sys.exc_info()
+                new_tb = info[2].tb_next  # https://stackoverflow.com/q/44813333
+                if new_tb is None:  # exception occurs inside this wrapper, not inside of getter_orig
+                    new_tb = _generate_new_tb(getter.func_code)
+                raise info[0], info[1], new_tb
+            finally:
+                del info
+
+        try:
+            update_wrapper(getter_new, getter)
+        except AttributeError:
+            pass
+        if inspect.isclass(obj):
+            if inspect.isfunction(src):
+                getter_new = staticmethod(getter_new)
+            elif getattr(src, '__self__', None) is not None:
+                getter_new = classmethod(getter_new)
+        setattr(obj, prop, getter_new)
+        return getter_orig
+    else:
+        return partial(overrideMethod, obj, prop)
+
 """
 IVM Carousel Handler
 """
