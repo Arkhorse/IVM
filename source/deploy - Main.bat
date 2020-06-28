ce25781e32034ef9232a41ac2c5e117b44e6b302
The Illusion (Arkhorse) <20694721+Arkhorse@users.noreply.github.com>
2020-06-28 08:56:47 -0300

    upd bat for 1.9.1.1, rem old hanger pyc

--- source/deploy - Main.bat
+++ source/deploy - Main.bat
@@ -4,6 +4,6 @@ cd "E:\Python\IVM\IVM\source\ivm"
 echo "Ziping and moving"
 @echo off
 7z a -tzip -mx0 com.ivm.wotmod "res"
-move /y "E:\Python\IVM\IVM\source\ivm\com.ivm.wotmod" "J:\World_of_Tanks_NA\mods\1.9.0.3\"
+move /y "E:\Python\IVM\IVM\source\ivm\com.ivm.wotmod" "J:\World_of_Tanks_NA\mods\1.9.1.1\"
 echo "Finished"
 pause
