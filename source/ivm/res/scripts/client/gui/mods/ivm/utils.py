d2e17591921bb37e59aa6b32c19bffa17a581018
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-07-04 13:38:29 -0300

    rem old non working code, add overrideMethod's, registerEvent

--- source/ivm/res/scripts/client/gui/mods/ivm/utils.py
+++ source/ivm/res/scripts/client/gui/mods/ivm/utils.py
@@ -1,59 +1,100 @@
-import inspect
-import sys
-from functools import partial, update_wrapper
+# Got from XVM
 
-#@overrideMethod
-def overrideMethod(obj, prop, getter=None, setter=None, deleter=None):
-    """
-    :param obj: object, which attribute needs overriding
-    :param prop: attribute name (can be not mangled), attribute must be callable
-    :param getter: fget function or None
-    :param setter: fset function or None
-    :param deleter: fdel function or None
-    :return function: unmodified getter or, if getter is None and src is not property, decorator"""
+#####################################################################
+# EventHook
 
-    if inspect.isclass(obj) and prop.startswith('__') and prop not in dir(obj) + dir(type(obj)):
-        prop = obj.__name__ + prop
-        if not prop.startswith('_'):
-            prop = '_' + prop
-    src = getattr(obj, prop)
-    if type(src) is property and (getter or setter or deleter):
-        props = []
-        for func, fType in ((getter, 'fget'), (setter, 'fset'), (deleter, 'fdel')):
-            assert func is None or callable(func), fType + ' is not callable!'
-            props.append(partial(func, getattr(src, fType)) if func else getattr(src, fType))
-        setattr(obj, prop, property(*props))
-        return getter
-    elif getter:
-        getter_orig = getter
-        assert callable(src), 'Source property is not callable!'
-        assert callable(getter_orig), 'Handler is not callable!'
-        while isinstance(getter, partial):
-            getter = getter.func
+class EventHook(object):
 
-        def getter_new(*a, **k):  # noinspection PyUnusedLocal
-            info = None
-            try:
-                return getter_orig(src, *a, **k)
-            except Exception:  # Code to remove this wrapper from traceback
-                info = sys.exc_info()
-                new_tb = info[2].tb_next  # https://stackoverflow.com/q/44813333
-                if new_tb is None:  # exception occurs inside this wrapper, not inside of getter_orig
-                    new_tb = _generate_new_tb(getter.func_code)
-                raise info[0], info[1], new_tb
-            finally:
-                del info
+    def __init__(self):
+        self.__handlers = []
 
-        try:
-            update_wrapper(getter_new, getter)
-        except AttributeError:
-            pass
-        if inspect.isclass(obj):
-            if inspect.isfunction(src):
-                getter_new = staticmethod(getter_new)
-            elif getattr(src, '__self__', None) is not None:
-                getter_new = classmethod(getter_new)
-        setattr(obj, prop, getter_new)
-        return getter_orig
+    def __iadd__(self, handler):
+        self.__handlers.append(handler)
+        return self
+
+    def __isub__(self, handler):
+        if handler in self.__handlers:
+            self.__handlers.remove(handler)
+        return self
+
+    def fire(self, *args, **keywargs):
+        for handler in self.__handlers:
+            handler(*args, **keywargs)
+
+    def clearObjectHandlers(self, inObject):
+        for theHandler in self.__handlers:
+            if theHandler.im_self == inObject:
+                self -= theHandler
+
+
+#####################################################################
+# Register events
+
+def _RegisterEvent(handler, cls, method, prepend=False):
+    evt = '__event_%i_%s' % ((1 if prepend else 0), method)
+    if hasattr(cls, evt):
+        e = getattr(cls, evt)
     else:
+<<<<<<< Updated upstream
         return partial(overrideMethod, obj, prop)
+=======
+        newm = '__orig_%i_%s' % ((1 if prepend else 0), method)
+        setattr(cls, evt, EventHook())
+        setattr(cls, newm, getattr(cls, method))
+        e = getattr(cls, evt)
+        m = getattr(cls, newm)
+        l = lambda *a, **k: __event_handler(prepend, e, m, *a, **k)
+        l.__name__ = method
+        setattr(cls, method, l)
+    e += handler
+
+def __event_handler(prepend, e, m, *a, **k):
+    try:
+        if prepend:
+            e.fire(*a, **k)
+            r = m(*a, **k)
+        else:
+            r = m(*a, **k)
+            e.fire(*a, **k)
+        return r
+    except:
+        print '[IVM] Error: __event_handler'
+def _override(cls, method, newm):
+    orig = getattr(cls, method)
+    if type(orig) is not property:
+        setattr(cls, method, newm)
+    else:
+        setattr(cls, method, property(newm))
+
+def _OverrideMethod(handler, cls, method):
+    orig = getattr(cls, method)
+    newm = lambda *a, **k: handler(orig, *a, **k)
+    newm.__name__ = method
+    _override(cls, method, newm)
+
+def _OverrideStaticMethod(handler, cls, method):
+    orig = getattr(cls, method)
+    newm = staticmethod(lambda *a, **k: handler(orig, *a, **k))
+    _override(cls, method, newm)
+
+def _OverrideClassMethod(handler, cls, method):
+    orig = getattr(cls, method)
+    newm = classmethod(lambda *a, **k: handler(orig, *a, **k))
+    _override(cls, method, newm)
+
+
+#####################################################################
+# Decorators
+
+def _hook_decorator(func):
+    def decorator1(*a, **k):
+        def decorator2(handler):
+            func(handler, *a, **k)
+        return decorator2
+    return decorator1
+    
+registerEvent = _hook_decorator(_RegisterEvent)
+overrideMethod = _hook_decorator(_OverrideMethod)
+overrideStaticMethod = _hook_decorator(_OverrideStaticMethod)
+overrideClassMethod = _hook_decorator(_OverrideClassMethod)
+>>>>>>> Stashed changes
