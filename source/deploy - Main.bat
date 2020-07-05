3765659e24627bc1454163ef80a94247e16a977c
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-07-04 21:32:37 -0300

    Update deploy - Main.bat

--- source/deploy - Main.bat
+++ source/deploy - Main.bat
@@ -1,9 +1,10 @@
+cls
 @echo off
 e:
 cd "E:\Python\IVM\IVM\source\ivm"
 echo "Ziping and moving"
 @echo off
-7z a -tzip -mx0 com.ivm.wotmod "res", "meta.xml", "LICENSE.txt"
-move /y "E:\Python\IVM\IVM\source\ivm\com.ivm.wotmod" "J:\World_of_Tanks_NA\mods\1.9.1.1\"
+7z a -tzip -mx0 com.ivm.wotmod "meta.xml" "LICENSE.txt" "res"
+move /y "E:\Python\IVM\IVM\source\ivm\com.ivm.wotmod" "H:\World_of_Tanks_NA\mods\1.9.1.1\"
 echo "Finished"
 pause
