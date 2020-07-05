32ef3d4fa398e4e7d98bb3515b7a2e8716f132c2
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-07-04 21:33:40 -0300

    add enableAutoSpeed + Reformated templates

--- source/ivm/res/scripts/client/gui/mods/ivm/battle.py
+++ source/ivm/res/scripts/client/gui/mods/ivm/battle.py
@@ -5,6 +5,8 @@ from gui.Scaleform.daapi.view.battle.shared.hint_panel.plugins import PreBattleH
 from gui.Scaleform.daapi.view.battle.shared.messages.fading_messages import FadingMessages
 from constants import ARENA_GUI_TYPE
 from Vehicle import Vehicle
+from gui.Scaleform.daapi.view.battle.shared.battle_timers import PreBattleTimer
+from constants import VEHICLE_SETTING
 
 from ..mod_ivm import Version
 from utils import overrideMethod, registerEvent
@@ -16,27 +18,35 @@ class ivmBattle(PYmodsConfigInterface):
         self.ID = 'ivmBattle'
         self.version = Version
         self.modsGroups = 'IVM'
-        self.data = {'enabled': True, 'questHintEnabled': True, 'notShowBattleMessage': True}
+        self.data = {'enabled': True, 'questHintEnabled': True, 'notShowBattleMessage': True, 'enableAutoSpeed': True}
         super(ivmBattle, self).init()
 
     def loadLang(self):
         if self.lang == 'en':
             self.i18n = {
+                # UI_setting__text
+                # UI_setting__tooltip
                 'name': 'Battle Options',
-                'UI_setting_questHintEnabled_text': 'This will disable the mission popup at the start of battle.',
-                'UI_setting_notShowBattleMessage_text': 'This will disable the messages for destroyed tanks above the minimap.'
+                'UI_setting_questHintEnabled_text': 'Disable Mission Popup.',
+                'UI_setting_questHintEnabled_tooltip': 'This will disable the mission popup at the start of battle.',
+                'UI_setting_notShowBattleMessage_text': 'Disable Destroyed Tanks Message.',
+                'UI_setting_notShowBattleMessage_tooltip': 'This will disable the messages for destroyed tanks above the minimap.',
+                'UI_setting_enableAutoSpeed_text': 'Start Battle In Speed Mode For Wheeled \"Tanks\".',
+                'UI_setting_enableAutoSpeed_tooltip': 'Always start in the speed mode for wheeled tanks with this.'
             }
     
     def createTemplate(self):
         return {
             'modDisplayName': self.i18n['name'],
             'enabled': self.data['enabled'],
-            'column1': [self.tb.createControl('questHintEnabled'), self.tb.createControl('notShowBattleMessage')]
+            'column1': [self.tb.createControl('questHintEnabled'), self.tb.createControl('notShowBattleMessage')],
+            'column2': [self.tb.createControl('enableAutoSpeed')]
         }
 
 config = ivmBattle()
 questHintEnabled = config.data['questHintEnabled']
 notShowBattleMessage = config.data['notShowBattleMessage']
+enableAutoSpeed = config.data['enableAutoSpeed']
 
 #oldQuestHint_WG = PreBattleHintPlugin._PreBattleHintPlugin__canDisplayQuestHint
 @overrideMethod(PreBattleHintPlugin, '_PreBattleHintPlugin__canDisplayQuestHint')
@@ -48,9 +58,8 @@ def ivmQuestHint(base, self):
     else:
         print '[IVM][LOAD] Missions Hint Panel Disabled'
         return None
-
-
 #PreBattleHintPlugin._PreBattleHintPlugin__canDisplayQuestHint = ivmQuestHint
+
 
 isFrontLine = False
 
@@ -68,3 +77,24 @@ def FadingMessages_showMessage(base, self, key, args=None, extra=None, postfix='
     # if not isFrontLine:
     #     return base(self, key, args, extra, postfix)
     pass
+
+isWheeledTech = False
+@registerEvent(Vehicle, 'onEnterWorld')
+def Vehicle_onEnterWorld(self, prereqs):
+    if not enableAutoSpeed:
+        return
+    global isWheeledTech
+    if self.isPlayerVehicle:
+        isWheeledTech = self.isWheeledTech and self.typeDescriptor.hasSiegeMode
+        if isWheeledTech:
+            arenaPeriod = BigWorld.player().guiSessionProvider.shared.arenaPeriod
+            startBattle = arenaPeriod.getPeriod() if arenaPeriod is not None else 0
+            if startBattle >= 3:
+                BigWorld.player().enableOwnVehicleAutorotation(True)
+
+@registerEvent(PreBattleTimer, 'hideCountdown')
+def hideCountdown(self, state, speed):
+    if not enableAutoSpeed:
+        return
+    if state == 3 and isWheeledTech:
+        BigWorld.player().base.vehicle_changeSetting(VEHICLE_SETTING.SIEGE_MODE_ENABLED, True)
