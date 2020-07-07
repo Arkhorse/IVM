ba37df924a21e7f7e09a123ca5e1f25180e9569e
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-07-07 12:09:25 -0300

    attempt to get systemMsg to work. Still WIP

--- source/ivm/res/scripts/client/gui/mods/mod_IVM.py
+++ source/ivm/res/scripts/client/gui/mods/mod_IVM.py
@@ -1,5 +1,14 @@
 import os
 
+from account import PlayerAccount
+from .ivm.Core import CORE
+from .ivm.fixes import *
+
+# CORE.printLoadMessage('IVM', '       ', buildDate='July 12th, 2020')
+
+if CORE.debug:
+    import platform
+    print '%s Debug info:\nCurrent OS Version is %s.\nCurrent mod build version is %s\nCurrent game version is %s.' % (CORE.ModIDShort, platform.version, CORE.Version[1], CORE.gameVersion)
 
 garage = True
 sound = True
@@ -7,31 +16,37 @@ battle = True
 
 try:
     from .ivm.ivmGarage import *
-    print ModIDShort, 'Garage Module Found'
+    print CORE.ModIDShort, 'Garage Module Found'
 except ImportError:
     garage = False
-    print ModIDShort, 'Garage Module Not Found'
+    print CORE.ModIDShort, 'Garage Module Not Found'
     pass
 
 try:
     from .ivm.sound import *
-    print ModIDShort, 'Sound Module Found'
+    print CORE.ModIDShort, 'Sound Module Found'
 except ImportError:
     sound = False
-    print ModIDShort, 'Sound Module Not Found'
+    print CORE.ModIDShort, 'Sound Module Not Found'
     pass
 
 try:
     from .ivm.battle import *
-    print ModIDShort, 'Battle Module Found'
+    print CORE.ModIDShort, 'Battle Module Found'
 except ImportError:
     battle = False
-    print ModIDShort, 'Battle Module Not Found'
+    print CORE.ModIDShort, 'Battle Module Not Found'
     pass
 
+from time import sleep
+
 if not garage or not sound or not battle:
-    SystemMessages.pushMessage(text='[IVM] Modules not found. Redownload Mod', type=SystemMessages.SM_TYPE.Error)
-    print ModIDShort, 'Modules Not Found, Redownload Mod @ %s' % (downloadURL)
+    CORE.addSystemMessage_Error('IVM Failed to load')
+    print CORE.ModIDShort, 'Modules Not Found, Redownload Mod @ %s' % (CORE.downloadURL)
 else:
-    SystemMessages.pushMessage(text='[IVM] Loaded. Have fun. Don\'t get artied', type=SystemMessages.SM_TYPE.Information)
-    print ModIDShort, 'Loaded', 'Version', Version[1]
+    CORE.addSystemMessage_GameGreeting('[IVM] Loaded. Have fun. Don\'t get artied')
+    print CORE.ModIDShort, 'Loaded', 'Version', CORE.Version[1]
+
+if CORE.debug:
+    CORE.addSystemMessage_GameGreeting('Debug Info:\nGarage = %s\nSound= %s\nBattle = %s' % (garage, sound, battle))
+    
