358586dfe9e450f33cd88b5283bbc542e94bdf75
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-07-09 15:21:21 -0300

    Update deploy.py

--- source/deploy.py
+++ source/deploy.py
@@ -1,4 +1,8 @@
 from compileall import compile_dir
+from shutil import copyfile as c
+from shutil import make_archive as m
 
 compile_dir('E:\Python\IVM\IVM\source\ivm')
 # a -tzip -mx0 IVM.Main.wotmod "res"
+
+#m('com.ivm', format='zip', root_dir='E:\Python\IVM\IVM\source\ivm', base_dir='E:\Python\IVM\IVM\source\ivm')
