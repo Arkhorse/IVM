f4ab15bafaa9cf70f14825d6adb206dd7286e8ee
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-08-03 14:57:52 -0300

    Finished Wot update 1.10.0.0

--- source/deploy - Main.bat
+++ source/deploy - Main.bat
@@ -1,7 +1,7 @@
 cls
 @echo off
 rem Set Variables
-set "versiondir=1.9.1.2"
+set "versiondir=1.10.0.0"
 set "Dependencies=E:\Python\IVM\IVM\Dependencies"
 set "gameDir=H:\World_of_Tanks_NA\"
 
@@ -18,14 +18,14 @@ xcopy /v /y "E:\Python\IVM\IVM\source\engine_config.xml" "%gameDir%\res_mods\%ve
 xcopy /v /y "E:\Python\IVM\IVM\source\audio\IVM.bnk" "%gameDir%\res_mods\%versiondir%\audioww\"
 xcopy /v /y "E:\Python\IVM\IVM\source\audio\audio_mods.xml" "%gameDir%\res_mods\%versiondir%\audioww\"
 
-xcopy /v /y "%Dependencies%\izeberg.modsettingsapi_1.3.0.wotmod" "%gameDir%\mods\%versiondir%\"
-xcopy /v /y "%Dependencies%\poliroid.modslistapi_1.3.2.wotmod" "%gameDir%\mods\%versiondir%\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\1.9.1.2\izeberg.modsettingsapi_1.3.0.wotmod" "%gameDir%\mods\%versiondir%\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\1.9.1.2\poliroid.modslistapi_1.3.2.wotmod" "%gameDir%\mods\%versiondir%\"
 
 rem xCopy will make these folders. Found not already having the mods and res_mods folders made at the start causes issues
-xcopy /v /y "%Dependencies%\PYmodsCore.wotmod" "%gameDir%\mods\%versiondir%\PYmods\"
-xcopy /v /y "%Dependencies%\en.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
-xcopy /v /y "%Dependencies%\de.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
-xcopy /v /y "%Dependencies%\ru.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\1.9.1.2\PYmods\PYmodsCore.wotmod" "%gameDir%\mods\%versiondir%\PYmods\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\configs\PYmods\PYmodsGUI\i18n\en.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\configs\PYmods\PYmodsGUI\i18n\de.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
+xcopy /v /y "E:\Python\IVM\IVM\Publish\mods\configs\PYmods\PYmodsGUI\i18n\ru.json" "%gameDir%\mods\configs\PYmods\PYmodsGUI\i18n\"
 
 echo "Finished"
 pause
